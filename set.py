d043784219fa98d92df9abd16c7c097a78616919
Matthew DuBois <147000029+duboimd@users.noreply.github.com>
2024-11-05 10:52:25 -0700

    Create set.py

---
+++ set.py
@@ -0,0 +1,15 @@
+print("Question 1:")
+# Q1: Create a random set of 5 sports called sports.
+# a. Print the set
+# b. Print the length of a set
+# c. Add the string “HELLO” to your set
+# d. Remove the first item in your set.
+
+print()
+print("Question 2:")
+# Q2: Given this set: 
+mySet = {"Blue", "Purple", "Pink", "Orange", "Blue", "Pink"}
+# a. Print mySet - using a comment, explain what you notice? What is wrong with mySet?
+
+
+
